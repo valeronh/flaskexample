bd887cef1ff782d216cf022b804c2272c1f4cdba
Valery <valeronh@gmail.com>
2022-09-17 01:33:53 +0300

    Wrap in class attendance_2_mysql

--- attendance_2_mysql.py
+++ attendance_2_mysql.py
@@ -1,47 +1,53 @@
 import mysql.connector
 import csv
 
+class Attendance2Mysql:
+    mydb = None
+    mycursor = None
 
-def open_connection():
-    mydb=mysql.connector.connect(
-        host="localhost",
-        user="valerys",
-        password="V@le1988",
-        database="valerydb"
-    )
-    return mydb
+    def __init__(self):
+        self.mydb = self.open_connection()
+        self.mycursor = self.mydb.cursor()
+        
 
-def select_owner():
-    print("Contents of the table: ")
-    mycursor.execute("SELECT * from owner")
-    print(mycursor.fetchall())
+    def open_connection(self):
+        mydb=mysql.connector.connect(
+            host="localhost",
+            user="valerys",
+            password="V@le1988",
+            database="valerydb"
+        )
+        return mydb
 
-def reset_owner():
-    try:
-        mycursor.execute("DELETE FROM owner")
-        mydb.commit()
-        mydb.flush()
-    except:
-        mydb.rollback()
+    def select_owner(self):
+        print("Contents of the table: ")
+        self.mycursor.execute("SELECT * from owner")
+        print(self.mycursor.fetchall())
 
-def insert_owner(name, average):
-    sql = "INSERT INTO owner (name, average) VALUES (%s, %s)"
-    val = (name, average)
-    mycursor.execute(sql, val)
-    mydb.commit()
+    def reset_owner(self):
+        try:
+            self.mycursor.execute("DELETE FROM owner")
+            self.mydb.commit()
+            self.mydb.flush()
+        except:
+            self.mydb.rollback()
 
-def read_csv(csvfile):
-    with open(csvfile, newline='') as file:
-        filereader = csv.reader(file, delimiter=',')
-        for row in filereader:
-            insert_owner(row[1], row[15])
+    def insert_owner(self, name, average):
+        sql = "INSERT INTO owner (name, average) VALUES (%s, %s)"
+        val = (name, average)
+        self.mycursor.execute(sql, val)
+        self.mydb.commit()
 
-mydb = open_connection()
-mycursor = mydb.cursor()
-if __name__ == "__main__":
-    select_owner()
-    reset_owner()
-    read_csv('attendance.csv')
-    select_owner()
-    mydb.close()
+    def read_csv(self, csvfile):
+        with open(csvfile, newline='') as file:
+            filereader = csv.reader(file, delimiter=',')
+            for row in filereader:
+                self.insert_owner(row[1], row[15])
+
+    def run_test(self):
+        self.select_owner()
+        self.reset_owner()
+        self.read_csv('attendance.csv')
+        self.select_owner()
+        self.mydb.close()
 
---
+++ attendance_test.py
@@ -0,0 +1,5 @@
+from attendance_2_mysql import Attendance2Mysql
+
+if __name__ == "__main__":
+    att = Attendance2Mysql()
+    att.run_test()
