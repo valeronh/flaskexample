ad8f1ce423d435770ef17e266d09870fcb53fab8
Valery <valeronh@gmail.com>
2022-09-18 01:28:17 +0300

    Add cloud connection

---
+++ cloud_connection.py
@@ -0,0 +1,33 @@
+import os
+import pysftp
+from dotenv import dotenv_values
+
+class CloudConnection:
+    config = None
+    
+    def __init__(self):
+        self.config = dotenv_values(".env")
+    
+    def list(self):
+        with pysftp.Connection(host=self.config["CLOUD_HOSTNAME"], username=self.config["CLOUD_USERNAME"], password=self.config["CLOUD_PASSWORD"]) as sftp:
+            print ("Connection succesfully stablished ... ")
+            sftp.cwd(self.config["CLOUD_PATH"])
+            directory_structure = sftp.listdir_attr()
+            folder_dict = {}
+            for attr in directory_structure:
+                 folder_dict[attr.filename] = attr
+            return folder_dict
+
+    def download_file(self, remoteFilePath, localFilePath):
+        with pysftp.Connection(host=self.config["CLOUD_HOSTNAME"], username=self.config["CLOUD_USERNAME"], password=self.config["CLOUD_PASSWORD"]) as sftp:
+            sftp.cwd(self.config["CLOUD_PATH"])
+            print ("Connection succesfully stablished ... ")
+            sftp.get(remoteFilePath, localFilePath)
+
+    def download_cloud_files(self):
+        list = self.list()
+        for key in list.keys():
+            self.download_file(key, "csv_files/" + key)
+
+    def run_test(self):
+        self.download_cloud_files()
---
+++ cloud_connection_test.py
@@ -0,0 +1,6 @@
+from cloud_connection import CloudConnection
+import sys
+
+if __name__ == "__main__":
+    att = CloudConnection()
+    att.run_test()
